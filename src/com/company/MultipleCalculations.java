df36ace769134e101b9fe4a548305db50dcd4b6e
Inta Slapna <Inta.slapnja@inbox.lv>
2021-09-22 21:44:49 +0300

    Finished 3rd Assignment

--- src/com/company/MultipleCalculations.java
+++ src/com/company/MultipleCalculations.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.net.Socket;
 import java.util.Scanner;
 
 public class MultipleCalculations {
@@ -10,14 +9,35 @@ public class MultipleCalculations {
         int X = scan.nextInt();
         System.out.println("Choose the meaning of Y:");
         int Y = scan.nextInt();
-        System.out.println("Choose the calculation type using one of theese symbols: +, -, /, *, %, p, b, s.");
+        System.out.println("Choose the calculation type using one of these symbols: +, -, /, *, %, p, b, s.");
         char operation = scan.next().charAt(0);
         switch (operation) {
             case '+':
                 System.out.println("The sum is: " + X + Y);
                 break;
             case '-':
-                System.out.println("The subtraction sum is: " + X - Y);
+                System.out.println("The subtraction sum is: " + (X-Y));
+                break;
+            case '/':
+                System.out.println("The result of dividing is: " + X/Y);
+                break;
+            case '*':
+                System.out.println("The result of multiplication is: " + X*Y);
+                break;
+            case '%':
+                System.out.println("The result of calculation is: " + X%Y);
+                break;
+            case 'p':
+                System.out.println("The value of of X is " +X + "and the value of Y is " +Y);
+                break;
+            case 'b':
+                System.out.println(X>Y ? "X is bigger":"Y is bigger");
+                break;
+            case 's':
+                System.out.println(X>Y ? "Y is smaller":"X is smaller");
+                break;
+            default:
+                System.out.println("Wrong calculation symbol");
                 break;
 
         }
